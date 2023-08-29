b545e636eb8dbce50cb5ee0a2cf3218e37a63718
Thomas Gehrmann <s6thgehr@gmail.com>
2023-08-29 17:23:05 +0200

    Implement spl_deposit instruction

--- rs/wba_vault/programs/wba_vault/src/lib.rs
+++ rs/wba_vault/programs/wba_vault/src/lib.rs
@@ -1,4 +1,8 @@
 use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token::{Mint, Token, TokenAccount},
+};
 
 declare_id!("B19SNU7Gm8VeeJ6GsQgjnzdXgC4QaxrX3EC4udk8HHF2");
 
@@ -6,6 +10,7 @@ declare_id!("B19SNU7Gm8VeeJ6GsQgjnzdXgC4QaxrX3EC4udk8HHF2");
 pub mod wba_vault {
     use super::*;
     use anchor_lang::system_program::{transfer, Transfer};
+    use anchor_spl::token::{transfer as spl_transfer, Transfer as SplTransfer};
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         ctx.accounts.state.auth_bump = *ctx.bumps.get("auth").unwrap();
@@ -47,6 +52,18 @@ pub mod wba_vault {
 
         transfer(cpi, amount)
     }
+
+    pub fn spl_deposit(ctx: Context<SPLDeposit>, amount: u64) -> Result<()> {
+        let accounts = SplTransfer {
+            from: ctx.accounts.owner.to_account_info(),
+            to: ctx.accounts.vault.to_account_info(),
+            authority: ctx.accounts.owner.to_account_info(),
+        };
+
+        let cpi = CpiContext::new(ctx.accounts.token_program.to_account_info(), accounts);
+
+        spl_transfer(cpi, amount)
+    }
 }
 
 #[derive(Accounts)]
@@ -71,6 +88,25 @@ pub struct Payment<'info> {
     vault: SystemAccount<'info>,
     #[account(seeds=[b"state", owner.key().as_ref()], bump = state.state_bump)]
     state: Account<'info, VaultState>,
+    system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct SPLDeposit<'info> {
+    #[account(mut)]
+    owner: Signer<'info>,
+    #[account(mut, associated_token::authority = owner, associated_token::mint = mint)]
+    owner_ata: Account<'info, TokenAccount>,
+    mint: Account<'info, Mint>,
+    #[account(seeds=[b"auth", state.key().as_ref()], bump)]
+    ///CHECK: This is safe
+    auth: UncheckedAccount<'info>,
+    #[account(init, seeds=[b"spl_vault", state.key().as_ref()], payer = owner, token::mint = mint, token::authority = auth, bump)]
+    vault: Account<'info, TokenAccount>,
+    #[account(seeds=[b"state", owner.key().as_ref()], bump = state.state_bump)]
+    state: Account<'info, VaultState>,
+    token_program: Program<'info, Token>,
+    associated_token_account: Program<'info, AssociatedToken>,
     system_program: Program<'info, System>,
 }
 
